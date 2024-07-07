3bb91b5c35ee26bdeaaabe95702809df841e1a3c
unknown <puneethkumarvh@gmail.com>
2024-07-07 10:59:50 +0530

    Add index methods to verify spring security

--- src/main/java/com/example/userCrud/controllers/IndexController.java
+++ src/main/java/com/example/userCrud/controllers/IndexController.java
@@ -10,4 +10,17 @@ public class IndexController {
     public String getTest(){
         return "Hello from get test controller.";
     }
+    @GetMapping("/")
+    public String getHome(){
+        return "Welcome to Home Page !";
+    }
+    @GetMapping("/app/admin")
+    public String getAdmin(){
+        return "Welcome Admin";
+    }
+
+    @GetMapping("app/dashboard")
+    public String getDashboard(){
+        return "Welcome to Dashboard!";
+    }
 }
